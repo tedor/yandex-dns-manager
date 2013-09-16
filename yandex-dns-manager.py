2cc4530d7367f79f405c0c384d93af32d1a08322
tedor <vetalyi@gmail.com>
2013-09-16 23:53:03 +0300

    Add main template

--- yandex-dns-manager.py
+++ yandex-dns-manager.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from flask.templating import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('main.html')
 
 
 if __name__ == '__main__':
